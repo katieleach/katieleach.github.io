e65582d162de5dda72e87b5c16ebf8806f5917bc
Katie Leach <kleach01@qub.ac.uk>
2015-07-31 09:58:49 +0100

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-	$.get('/katieleach.github.io/templates/navbar.html', function(data) {
+	$.get('/templates/navbar.html', function(data) {
 		$('#page_wrap').prepend(data);
 	});
-	$.get('/katieleach.github.io/templates/footer.html', function(data) {
+	$.get('/templates/footer.html', function(data) {
 		$('#page_wrap').append(data);
 	});
 });
